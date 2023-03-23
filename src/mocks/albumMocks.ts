522247e9acaec0e11187cf5ccbdb9f519433c516
tecxh <techno.zelda@gmail.com>
2023-03-22 21:09:57 -0400

    album mock & type

---
+++ src/mocks/albumMocks.ts
@@ -0,0 +1,40 @@
+import { Album } from "../types";
+
+export const mockAlbums: Album[] = [
+    {
+        _id: '7',
+        title: '10,000 gecs',
+        artist: '100 gecs',
+        numberOfTracks: 8,
+    },
+    {
+        _id: '8',
+        title: 'The Con',
+        artist: 'Tegan and Sara',
+        numberOfTracks: 11,
+    },
+    {
+        _id: '9',
+        title: 'Deathconciousness',
+        artist: 'Have a Nice Life',
+        numberOfTracks: 10,
+    },
+    {
+        _id: '10',
+        title: 'Fishmonger',
+        artist: 'underscores',
+        numberOfTracks: 12,
+    },
+    {
+        _id: '11',
+        title: 'effective. Power',
+        artist: 'MIMIDEATH',
+        numberOfTracks: 6,
+    },
+    {
+        _id: '12',
+        title: 'Favourite Worst Nightmare',
+        artist: 'Arctic Monkeys',
+        numberOfTracks: 0,
+    },
+]
---
+++ src/types/albums.ts
@@ -0,0 +1,6 @@
+export interface Album {
+  _id: string;
+  title: string;
+  artist: string;
+  numberOfTracks: number;
+} 
