ce85a17518e39ad11bdb1f950ccbb3400880135b
tecxh <techno.zelda@gmail.com>
2023-03-22 21:10:13 -0400

    flesh out album list a bit more

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import './App.css';
 import { AlbumList, SongList } from './components';
 
 function App() {
-  const handleSaveSong = (songId: string) => {
-    console.info(`todo - save song ${songId}`)
+  const handleContentSave = (contentId: string) => {
+    console.info(`todo - save song ${contentId}`)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <SongList onSave={handleSaveSong} />
-        <AlbumList />
+        <SongList onSave={handleContentSave} />
+        <AlbumList onSave={handleContentSave} />
       </header>
     </div>
   );
--- src/components/AlbumList/AlbumList.tsx
+++ src/components/AlbumList/AlbumList.tsx
@@ -1,9 +1,24 @@
-export const AlbumList = () => {
-  const placeholder = 'albumlist';
+import { mockAlbums } from "../../mocks/albumMocks";
+
+interface AlbumListProps {
+  onSave: (albumId: string) => void;
+}
+
+export const AlbumList = ({ onSave }: AlbumListProps) => {
 
   return (
-    <p>
-      {placeholder}
-    </p>
+    <div style={{ display: 'flex', flexDirection: 'row', columnGap: 20 }}>
+      {mockAlbums.map((album) => {
+        const { title, artist, numberOfTracks, _id } = album;
+
+        return (
+          <div style={{ backgroundColor: 'black', padding: 16 }}>
+            <p>{`${title} - ${artist}`}</p>
+            <p>{`Track length: ${numberOfTracks}`}</p>
+            <button onClick={() => onSave(_id)} style={{ backgroundColor: 'pink' }}>save</button>
+          </div>
+        )
+      })}
+    </div>
   )
 }
