dd6dee23a161ba516d81417278b15e872feda5c1
tecxh <techno.zelda@gmail.com>
2023-03-22 20:59:46 -0400

    map songs, todo handler

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import './App.css';
 import { AlbumList, SongList } from './components';
 
 function App() {
+  const handleSaveSong = (songId: string) => {
+    console.info(`todo - save song ${songId}`)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <SongList />
+        <SongList onSave={handleSaveSong} />
         <AlbumList />
       </header>
     </div>
--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -1,9 +1,22 @@
-export const SongList = () => {
-  const placeholder = 'songList';
+import { mockSongs } from '../../mocks';
 
+interface SongListProps {
+  onSave: (songId: string) => void;
+}
+
+export const SongList = ({ onSave }: SongListProps) => {
   return (
-    <p>
-      {placeholder}
-    </p>
+    <div style={{ display: 'flex', flexDirection: 'row', columnGap: 20 }}>
+      {mockSongs.map((song) => {
+        const { title, artist, _id } = song;
+
+        return (
+          <div key={_id} style={{ backgroundColor: 'black', padding: 16 }}>
+            <p>{`${title} - ${artist}`}</p>
+            <button onClick={() => onSave(_id)} style={{ backgroundColor: 'pink' }}>save</button>
+          </div>
+        )
+      })}
+    </div>
   )
 }
