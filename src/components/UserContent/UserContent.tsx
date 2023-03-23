451e9f030373de40a8e0134b8428eb55c64ca74d
tecxh <techno.zelda@gmail.com>
2023-03-22 22:15:54 -0400

    use xstate

--- src/components/UserContent/UserContent.tsx
+++ src/components/UserContent/UserContent.tsx
@@ -1,31 +1,31 @@
+import { useMachine } from "@xstate/react";
 import { useContext, useState } from "react";
 
 import { UserContext } from "../../context/userContext";
 import { AlbumList } from "../AlbumList"
 import { SongList } from "../SongList"
-
-enum UserContentTabs {
-  Songs = 'songs',
-  Albums = 'albums',
-}
+import { userContentMachine, UserContentMachineActions, UserContentMachineStates } from "./userContentMachine";
 
 export const UserContent = () => {
-  const [activeTab, setActiveTab] = useState<UserContentTabs>(UserContentTabs.Albums)
+  const [state, dispatch] = useMachine(userContentMachine);
+  const activeTab = state.value;
   const { saves: { albums, songs } } = useContext(UserContext)
+
+  const toggle = () => dispatch({ type: UserContentMachineActions.Toggle })
 
   return (
     <div>
       <h2>User Content</h2>
-      <button onClick={() => setActiveTab(UserContentTabs.Albums)}>Albums</button>
-      <button onClick={() => setActiveTab(UserContentTabs.Songs)}>Songs</button>
+      <button onClick={toggle}>Albums</button>
+      <button onClick={toggle}>Songs</button>
       <hr />
-      {activeTab === UserContentTabs.Songs && (
+      {activeTab === UserContentMachineStates.Songs && (
         <div>
           <h3>Songs</h3>
           <SongList songs={songs} />
         </div>
       )}
-      {activeTab === UserContentTabs.Albums && (
+      {activeTab === UserContentMachineStates.Albums && (
         <div>
           <h3>Albums</h3>
           <AlbumList albums={albums} />
