93f8bd5368b545aee75168c9cdbb390d50045381
tecxh <techno.zelda@gmail.com>
2023-03-22 21:36:41 -0400

    use context; set up tabs between songs & albums

--- src/components/UserContent/UserContent.tsx
+++ src/components/UserContent/UserContent.tsx
@@ -1,19 +1,36 @@
+import { useContext, useState } from "react";
+
+import { UserContext } from "../../context/userContext";
 import { AlbumList } from "../AlbumList"
 import { SongList } from "../SongList"
 
-export const UserContent = () => {
-    const { songs, albums } = (() => {
-        return {
-            songs: [],
-            albums: []
-        }
-    })();
+enum UserContentTabs {
+  Songs = 'songs',
+  Albums = 'albums',
+}
 
-    return (
+export const UserContent = () => {
+  const [activeTab, setActiveTab] = useState<UserContentTabs>(UserContentTabs.Albums)
+  const { saves: { albums, songs } } = useContext(UserContext)
+
+  return (
+    <div>
+      <h2>User Content</h2>
+      <button onClick={() => setActiveTab(UserContentTabs.Albums)}>Albums</button>
+      <button onClick={() => setActiveTab(UserContentTabs.Songs)}>Songs</button>
+      <hr />
+      {activeTab === UserContentTabs.Songs && (
         <div>
-            <h2>User Content</h2>
-            <SongList songs={songs} />
-            <AlbumList albums={albums} />
+          <h3>Songs</h3>
+          <SongList songs={songs} />
         </div>
-    )
+      )}
+      {activeTab === UserContentTabs.Albums && (
+        <div>
+          <h3>Albums</h3>
+          <AlbumList albums={albums} />
+        </div>
+      )}
+    </div>
+  )
 }
